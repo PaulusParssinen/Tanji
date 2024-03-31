5ed4dade6a5c1571416dc1d45cd612e91c895dde
Adolfo Huitron <adolfo@huitron.dev>
2024-03-30 20:36:01 -0700

    chore: add service for handling remote endpoint resolution

---
+++ Tanji.Infrastructure/Services/IRemoteEndPointResolverService.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+using Tanji.Core.Net;
+
+namespace Tanji.Infrastructure.Services;
+
+public interface IRemoteEndPointResolverService<TEndPoint> where TEndPoint : IPEndPoint
+{
+    Task<TEndPoint> ResolveAsync(string ticket, CancellationToken cancellationToken = default);
+    Task<TEndPoint> ResolveAsync(HNode local, HConnectionContext context, CancellationToken cancellationToken = default);
+}
